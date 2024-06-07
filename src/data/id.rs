6aa85e24dabfdcaa1d9ba787ee14d7fe38a34cb9
JohanVonElectrum <joanestradium@gmail.com>
2024-06-07 17:18:40 +0200

    changed id bits

--- src/data/id.rs
+++ src/data/id.rs
@@ -2,6 +2,21 @@ use std::time::{Duration, SystemTime};
 use rand::Rng;
 use rand::rngs::ThreadRng;
 
+const TIMESTAMP_BITS: u8 = 64;
+const SEQUENCE_BITS: u8 = 12;
+const SERVICE_ID_BITS: u8 = 16;
+const WORKER_ID_BITS: u8 = 16;
+const RANDOM_BITS: u8 = 20;
+
+const _: () = assert!(TIMESTAMP_BITS + SEQUENCE_BITS + SERVICE_ID_BITS + WORKER_ID_BITS + RANDOM_BITS == 128);
+
+const TIMESTAMP_OFFSET: u8 = SEQUENCE_OFFSET + SEQUENCE_BITS;
+const SEQUENCE_OFFSET: u8 = SERVICE_ID_OFFSET + SERVICE_ID_BITS;
+const SERVICE_ID_OFFSET: u8 = WORKER_ID_OFFSET + WORKER_ID_BITS;
+const WORKER_ID_OFFSET: u8 = RANDOM_BITS;
+
+const _: () = assert!(128 == TIMESTAMP_OFFSET + TIMESTAMP_BITS);
+
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub struct Identifier {
     timestamp: SystemTime,
@@ -14,10 +29,10 @@ pub struct Identifier {
 impl From<u128> for Identifier {
     fn from(id: u128) -> Self {
         Self {
-            timestamp: SystemTime::UNIX_EPOCH + Duration::from_millis((id >> 64) as u64),
-            service_id: ((id >> 48) & 0xffff) as u16,
-            worker_id: ((id >> 32) & 0xffff) as u16,
-            sequence: ((id >> 16) & 0xffff) as u16,
+            timestamp: SystemTime::UNIX_EPOCH + Duration::from_millis((id >> TIMESTAMP_OFFSET) as u64),
+            sequence: ((id >> SEQUENCE_OFFSET) & 0xffff) as u16,
+            service_id: ((id >> SERVICE_ID_OFFSET) & 0xffff) as u16,
+            worker_id: ((id >> WORKER_ID_BITS) & 0xffff) as u16,
             random: (id & 0xffff) as u16,
         }
     }
@@ -25,10 +40,10 @@ impl From<u128> for Identifier {
 
 impl From<Identifier> for u128 {
     fn from(id: Identifier) -> u128 {
-        (id.timestamp.duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis() as u128) << 64 |
-            (id.service_id as u128) << 48 |
-            (id.worker_id as u128) << 32 |
-            (id.sequence as u128) << 16 |
+        id.timestamp.duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis() << TIMESTAMP_OFFSET |
+            (id.sequence as u128) << SEQUENCE_OFFSET |
+            (id.service_id as u128) << SERVICE_ID_OFFSET |
+            (id.worker_id as u128) << WORKER_ID_BITS |
             id.random as u128
     }
 }
@@ -69,10 +84,10 @@ impl IdentifierGenerator {
         self.timestamp = timestamp;
         self.sequence = sequence;
         let random = self.rng.gen::<u16>();
-        (timestamp as u128) << 64 |
-            (self.service_id as u128) << 48 |
-            (self.worker_id as u128) << 32 |
-            (sequence as u128) << 16 |
+        (timestamp as u128) << TIMESTAMP_OFFSET |
+            (sequence as u128) << SEQUENCE_OFFSET |
+            (self.service_id as u128) << SERVICE_ID_OFFSET |
+            (self.worker_id as u128) << WORKER_ID_BITS |
             random as u128
     }
 }
